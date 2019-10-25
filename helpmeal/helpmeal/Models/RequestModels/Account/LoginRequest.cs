7a14562bad876fe7f12befb266d44792cc564333
Gábor Trajtler <trajtlerg@gmail.com>
2019-10-25 15:28:29 +0200

    Add UserService

--- helpmeal/helpmeal/Models/RequestModels/Account/LoginRequest.cs
+++ helpmeal/helpmeal/Models/RequestModels/Account/LoginRequest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,15 @@ namespace helpmeal.Models.RequestModels.Account
 {
     public class LoginRequest
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
     }
 }
