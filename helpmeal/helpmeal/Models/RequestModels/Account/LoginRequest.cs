69482e9ba057ddde2b94f9c5572caa64028990ed
Gábor Trajtler <trajtlerg@gmail.com>
2019-10-25 15:08:08 +0200

    Add Google button

---
+++ helpmeal/helpmeal/Models/RequestModels/Account/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helpmeal.Models.RequestModels.Account
+{
+    public class LoginRequest
+    {
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+    }
+}
