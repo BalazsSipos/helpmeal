60ab5fb52dcadb79137d53e542fc03c9b5c0d081
Peter <babics.peter@gmail.com>
2019-10-25 14:49:00 +0200

    seeding meals and ingridients, no models

---
+++ helpmeal/helpmeal/ApplicationDbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helpmeal
+{
+    public class ApplicationDbInitializer
+    {
+        public static void SeedIngredients()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                UserName = "SuperUser",
+                Email = "superUser@gmail.com"
+            };
+        }
+        public static void SeedMeals()
+        {
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = "SuperUser",
+                    Email = "superUser@gmail.com"
+                };
+            }
+        }
+    }
+}
+
+
+
--- helpmeal/helpmeal/Startup.cs
+++ helpmeal/helpmeal/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodService.Services.BlobService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -22,12 +23,15 @@ namespace helpmeal
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IBlobStorageService, BlobStorageService>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            ApplicationDbInitializer.SeedMeals();
+            ApplicationDbInitializer.SeedIngredients();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
