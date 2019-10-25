950dc128c227cb7a20ca8dea575647ccc749e185
Gábor Trajtler <trajtlerg@gmail.com>
2019-10-25 19:41:53 +0200

    Start implementing shoppinglist

---
+++ helpmeal/helpmeal/Models/ViewModels/ShoppingListViewModels/ShoppingListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helpmeal.Models.ViewModels.ShoppingListViewModels
+{
+    public class ShoppingListViewModel
+    {
+        public List<Meal> Meals { get; set; }
+    }
+}
