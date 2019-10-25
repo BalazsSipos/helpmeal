3d90d1631646a47f477de4a85a9148b5a8f21787
Honffy Daniel <honffy.daniel@gmail.com>
2019-10-25 16:54:19 +0200

    Add MealService and GetMealListByCycleAndUserDayAsync

---
+++ helpmeal/helpmeal/Services/MealService/IMealService.cs
@@ -0,0 +1,14 @@
+﻿using helpmeal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace helpmeal.Services.MealService
+{
+    public interface IMealService
+    {
+        Task<List<Meal>> GetMealListByCycleAndUserDayAsync(Meal meal, ClaimsPrincipal user);
+    }
+}
---
+++ helpmeal/helpmeal/Services/MealService/MealService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using FoodService.Services.BlobService;
+using helpmeal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace helpmeal.Services.MealService
+{
+    public class MealService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        private readonly IMealService mealService;
+        private readonly IMapper mapper;
+        IBlobStorageService blobStorageService;
+
+        public MealService(ApplicationDbContext applicationDbContext, IMapper mapper, IBlobStorageService blobStorageService, IMealService mealService)
+        {
+            this.applicationDbContext = applicationDbContext;
+            this.mealService = mealService;
+            this.mapper = mapper;
+            this.blobStorageService = blobStorageService;
+        }
+
+        public async Task<List<Meal>> GetMealListByCycleAndUserDayAsync(Meal meal, ClaimsPrincipal user)
+        {
+            var mealList = await applicationDbContext.Meals.Where(cd => cd.CycleDay.Equals(meal.CycleDay)).Where(r => r.User.UserName == user.Identity.Name).ToListAsync();
+            return mealList;
+        }
+
+
+    }
+}
