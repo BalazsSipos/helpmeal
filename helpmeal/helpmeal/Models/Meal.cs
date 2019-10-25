09891de6da8c3c670df7b9c375549a9b45ee1df0
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-10-25 15:17:24 +0200

    Create initial model structure

---
+++ helpmeal/helpmeal/Models/Meal.cs
@@ -0,0 +1,18 @@
+﻿using helpmeal.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helpmeal.Models
+{
+    public class Meal
+    {
+        public long MealId { get; set; }
+        public AppUser User { get; set; }
+        public byte CycleDay { get; set; }
+        public Recipe Recipe { get; set; }
+        public int Amount { get; set; }
+        public DateTime SpecialDate { get; set; }
+    }
+}
--- helpmeal/helpmeal/Startup.cs
+++ helpmeal/helpmeal/Startup.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using helpmeal.Models.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,7 +25,43 @@ namespace helpmeal
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                services.AddDbContext<ApplicationDbContext>(build =>
+                {
+                    build.UseMySql(configuration.GetConnectionString("AzureConnection"));
+                });
+                // Automatically perform database migration
+                services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(build =>
+                {
+                    build.UseMySql(configuration.GetConnectionString("DefaultConnection"));
+                });
+            }
+
             services.AddMvc();
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = "";
+                    options.ClientSecret = "";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +73,8 @@ namespace helpmeal
             }
 
             app.UseStaticFiles();
-            app.UseMvc();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
