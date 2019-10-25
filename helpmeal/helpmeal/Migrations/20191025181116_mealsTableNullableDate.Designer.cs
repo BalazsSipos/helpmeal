6516bb648431451cabdcd83de78388ca3907ae61
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-10-25 20:28:55 +0200

    small changes

--- helpmeal/helpmeal/Migrations/20191025181116_mealsTableNullableDate.Designer.cs
+++ helpmeal/helpmeal/Migrations/20191025181116_mealsTableNullableDate.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using helpmeal;
 
 namespace helpmeal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191025181116_mealsTableNullableDate")]
+    partial class mealsTableNullableDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
